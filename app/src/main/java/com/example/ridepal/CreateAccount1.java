d4a3997091ff9c74b35cf1e1b7b13dee3fefb58a
Nisha0829 <sharmanisha0829@gmail.com>
2019-02-18 11:10:39 -0500

    Changes of Kyle

---
+++ app/src/main/java/com/example/ridepal/CreateAccount1.java
@@ -0,0 +1,13 @@
+package com.example.ridepal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAccount1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account1);
+    }
+}
